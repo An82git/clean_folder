f38bf90d02debca5aea713bb8fcba21edefef252
An82git <grugorchyk82@gmail.com>
2023-09-24 02:46:45 +0300

    Commit_3

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='clean_folder',
-    version='1',
+    version='1.1',
     description='Sorts files and removes empty folders',
     url='https://github.com/An82git/clean_folder.git',
     author='An82git',
     license='MIT',
     packages=find_namespace_packages(),
-    install_requires=['shutil', 'pathlib', 'sys', 're'],
-    entry_points={'console_scripts': ['clean-folder = clean-folder.clean:main']}
+    entry_points={'console_scripts': ['clean_folder = clean_folder.clean:main']}
     )
